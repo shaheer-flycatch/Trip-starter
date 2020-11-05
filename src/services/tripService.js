f4618a9a83f8929952ef92b7cea5c414d268d374
User <shaheer.m@flycatchtech.com>
2020-11-05 18:29:20 +0530

    refactor

--- src/services/tripService.js
+++ src/services/tripService.js
@@ -9,6 +9,8 @@ const handleResponse = (response) => {
      return response.data;
   };
 
+//get all trips
+
 function tripList() {
     return axios
       .get(BASE_URL)
@@ -16,24 +18,30 @@ function tripList() {
       .catch((error) => handleResponse(error.response));
   }
 
+  //delete trip
   function deleteTrip(id) {
     return axios
       .delete(`${BASE_URL}/${id}`)
       .then((response) => handleResponse(response))
       .catch((error) => handleResponse(error.response));
   }
+  //update trip
   function updateTrip(updatedTripData) {
     return axios
       .put(`${BASE_URL}/${updatedTripData.id}`,updatedTripData)
       .then((response) => handleResponse(response))
       .catch((error) => handleResponse(error.response));
   }
+
+  //create trip
   function createTrip(tripData) {
     return axios
       .post(BASE_URL,tripData)
       .then((response) => handleResponse(response))
       .catch((error) => handleResponse(error.response));
   }
+
+  //get trip by id
   function getTripById(id) {
     return axios
       .get(`${BASE_URL}/${id}`)
